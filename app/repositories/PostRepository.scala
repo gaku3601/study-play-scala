8832cdc2869e45b7ff5e599bfb80d3e848146d2b
gaku <pro.gaku@gmail.com>
2020-07-13 06:20:14 +0900

    リファクタ

--- app/repositories/PostRepository.scala
+++ app/repositories/PostRepository.scala
@@ -4,9 +4,6 @@ import models.Post
 import scalikejdbc._
 
 object PostRepository {
-
-  var posts: Seq[Post] = Vector()
-
   def findAll: Seq[Post] = DB readOnly { implicit session =>
     sql"SELECT id, body FROM posts".map { rs =>
       Post(rs.int("id"), rs.string("body"))
