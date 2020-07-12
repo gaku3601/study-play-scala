efa8a2e8259e75feafb7420ad8587e5a79739bcd
gaku <pro.gaku@gmail.com>
2020-07-12 12:53:12 +0900

    ログを仕込む

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,11 +1,14 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.api.Logger
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
 
 @Singleton
 class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
-  def get(name: Option[String]) = Action {implicit request: Request[AnyContent] =>
+  val logger: Logger = play.api.Logger("play")
+  def get(name: Option[String]): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    logger.info(s"name parameter: $name")
     Ok {
       name.map(s => s"Hello, $s!")
         .getOrElse("""Please give a name as a query parameter named "name".""")
