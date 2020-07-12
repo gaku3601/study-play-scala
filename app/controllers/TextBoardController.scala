ce3557403d4c8c73595a410ab23810deaa527cf6
gaku <pro.gaku@gmail.com>
2020-07-12 16:19:47 +0900

    一覧表示、投稿機能を実装

---
+++ app/controllers/TextBoardController.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import java.time.OffsetDateTime
+
+import javax.inject.{Inject, Singleton}
+import play.api.data.Form
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+
+case class PostRequest(body: String)
+
+@Singleton
+class TextBoardController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with I18nSupport {
+  private[this] val form = Form(
+    mapping(
+      "post" -> text(minLength = 1, maxLength = 10)
+    )(PostRequest.apply)(PostRequest.unapply))
+
+  def get: Action[AnyContent] = Action { implicit request =>
+    Ok(views.html.index(PostRepository.findAll, form))
+  }
+
+  def post: Action[AnyContent] = Action { implicit request =>
+    form.bindFromRequest().fold(
+      formWithErrors => BadRequest(views.html.index(PostRepository.findAll, formWithErrors)),
+      postRequest => {
+        val post = Post(postRequest.body, OffsetDateTime.now)
+        PostRepository.add(post)
+        Redirect("/text-board")
+      }
+    )
+  }
+}
