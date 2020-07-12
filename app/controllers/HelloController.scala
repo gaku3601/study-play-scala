d890841b501fd9e011b56b815dd28a194adf6f92
gaku <pro.gaku@gmail.com>
2020-07-12 12:36:29 +0900

    singletonアノテーションの追加

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,8 +1,9 @@
 package controllers
 
-import javax.inject.Inject
+import javax.inject.{Inject, Singleton}
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
+@Singleton
 class HelloController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
   def get(name: Option[String]) = Action {implicit request: Request[AnyContent] =>
     Ok {
